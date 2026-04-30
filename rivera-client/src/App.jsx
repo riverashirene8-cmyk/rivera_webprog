0ee84696ad1f9388b9f8169b72ff9cadec50908c
riverashirene8-cmyk <riverashirene8@gmail.com>
2026-04-30 08:54:16 +0800

    lab-act-instance

---
+++ rivera-client/src/App.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div id="center">
+      <div className="App">
+        <header className="App-header">
+          <h1>Welcome to My React App!</h1>
+          <p>
+            Name: [Shirene Rivera]<br />
+            Email: [riverashirene8@gmail.com]<br />
+            Sex: [Female]<br />
+            Birthday: [October 6, 2004]<br />
+            Age: [21 years old]<br />
+            Other Personal Info:
+          </p>
+        </header>
+      </div>
+    </div>
+  );
+}
+
+export default App;
